21ec2ac123bb4d82deb197ec88e399eaaab79c7d
Carlos Abreu <carlos@abreuh.com>
2021-06-14 23:27:51 -0400

    Style Changes

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,41 +1,13 @@
 import React from 'react';
+
+import Directory from '../../components/directory/directory.components';
+
 import './homepage.styles.scss';
+
 const HomePage = () => (
   <div className='homepage'>
-    <div className='directory-menu'>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='tittle'>HATS</h1>
-          <span className='subtittle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='tittle'>JACKETS</h1>
-          <span className='subtittle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='tittle'>SNEAKERS</h1>
-          <span className='subtittle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='tittle'>WOMEN</h1>
-          <span className='subtittle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='tittle'>MEN</h1>
-          <span className='subtittle'>SHOP NOW</span>
-        </div>
-      </div>
-    </div>
+    <Directory />
   </div>
 );
-
 
 export default HomePage;
